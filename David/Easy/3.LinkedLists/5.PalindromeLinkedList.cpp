e5afd091c657050cecad3c7c4fd1f9cb9adb04e9
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-03-21 13:43:44 -0600

    Initial setup

---
+++ David/Easy/3.LinkedLists/5.PalindromeLinkedList.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode * next;
+
+    ListNode(): val(0), next(nullptr){};
+    ListNode(int x): val(x), next(nullptr){};
+};
