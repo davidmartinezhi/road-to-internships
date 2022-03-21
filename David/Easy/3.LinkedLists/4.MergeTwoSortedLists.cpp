7fbcdef66fd39c664a6ed35fd09f8d250879c424
Gerardo Martínez <63136178+gerardomartinezhi@users.noreply.github.com>
2022-03-21 13:41:13 -0600

    Initial setup

---
+++ David/Easy/3.LinkedLists/4.MergeTwoSortedLists.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode{
+    int val;
+    ListNode * next;
+
+    ListNode(): val(0), next(nullptr){};
+    ListNode(int x): val(x), next(nullptr){};
+};
+
